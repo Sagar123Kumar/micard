44e99e414d0d90a53b205cb892d6586ee86cc4d2
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2023-03-05 20:41:08 +0530

    home

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "I Am Rich",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+// Container can only have one child text
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text("Container 1"),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Text("Container 2"),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.redAccent,
+                child: Text("Container 3"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
